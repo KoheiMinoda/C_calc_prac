6709a95c51f7dd76950199e65697ec95a5f4ac0a
Minoda Kohei <111864712+KoheiMinoda@users.noreply.github.com>
2023-07-27 10:13:49 +0900

    Add files via upload

---
+++ calc_first.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    cout << 2 << '+' << 3 << '=' << 2+3 << endl;
+    cout << 2 << '-' << 3 << '=' << 2-3 << endl;
+    cout << 2 << '*' << 3 << '=' << 2*3 << endl;
+    cout << 2 << '/' << 3 << '=' << 2/3 << endl;
+    cout << 9 << '+' << 3 << '=' << 9/3 << endl;
+}
+
