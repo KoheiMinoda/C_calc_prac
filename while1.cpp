f66e9ac018b14f74f8c597b9b8fe0381aa216c73
Minoda Kohei <111864712+KoheiMinoda@users.noreply.github.com>
2023-08-02 22:33:37 +0900

    Add files via upload

---
+++ while1.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+// int main() {
+//     int a = 0;
+//     while (a < 10) {
+//         cout  << a << ' ';
+//         a += 2;
+//     }
+//     cout << endl;
+// }
+
+int main() {
+    int i = 0;
+    do {
+        cout  << "この文は表示されるかどうか" << endl;
+    } while (i == 1);
+}
